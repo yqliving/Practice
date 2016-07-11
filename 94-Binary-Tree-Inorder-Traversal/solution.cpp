390e036b948cd13121cf447d12468dd55dec9e80
Xiaoying <yqliving@gmail.com>
2016-07-11 16:13:27 -0700

    [94. Binary Tree Inorder Traversal][Runtime Error]committed by LeetCode Extension

--- 94-Binary-Tree-Inorder-Traversal/solution.cpp
+++ 94-Binary-Tree-Inorder-Traversal/solution.cpp
@@ -8,46 +8,24 @@
  * };
  */
 class Solution {
-public:
-    vector<int> inorderTraversal(TreeNode* root)  {
-    vector<int> list;
-    vector<TreeNode*> stack;
-    TreeNode* cur = root;
-
-    while(cur!=NULL || !stack.empty()){
-        while(cur!=NULL){
-            stack.push_back(cur);
-            cur = cur->left;
-        }
-        cur = stack.pop_back();
-        list.push_back(cur->val);
-        cur = cur->right;
-    }
-
-    return list;
-}
-};
-/*
-class Solution {
-public:
-    vector<int> inorderTraversal(TreeNode* root) 
-	{
+    public:
+    vector<int> inorderTraversal(TreeNode* root) {
         vector<int> result;
-        if(root == NULL)
-            return result;
+        if(root == NULL) return result;
+        
         vector<TreeNode*> nodeStack;
         TreeNode* node = root;
         nodeStack.push_back(node);
         node = node->left;
-        while(!nodeStack.empty() || node != NULL) 
-		{//walk through left child node at first
-            if(node != NULL) 
-			{
+        
+        while (!nodeStack.empty() || node != NULL) {
+            //walk through left child node at first
+            if (node != node) {
                 nodeStack.push_back(node);
                 node = node->left;
-            }
-            else 
-			{//if there is no left child anymore, pop up current top node and add it to result
+            } 
+            //if there is no left child anymore, pop up current top node and add it to result
+            else {
                 node = nodeStack.back();
                 nodeStack.pop_back();
                 result.push_back(node->val);
@@ -57,5 +35,6 @@ public:
         return result;
     }
 };
+
 //stack
 //中序遍历，顺时针
