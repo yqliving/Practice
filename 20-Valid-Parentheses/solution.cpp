c3ee87861b451d0e350587228cbd16557dc03f3c
Xiaoying <yqliving@gmail.com>
2016-07-11 14:16:41 -0700

    [20. Valid Parentheses][Compile Error]committed by LeetCode Extension

--- 20-Valid-Parentheses/solution.cpp
+++ 20-Valid-Parentheses/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
     public:
     bool isValid (string s) {
     stack<char> temp;
-    map<char, char> m = {{']','['}, {')','('}, {'}','{'}};
+    map<char, char> m = { {']','['},{')','('},{'}','{'} };
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == '[' || s[i] == '{' || s[i] == '(') 
         temp.push(s[i]);                      
