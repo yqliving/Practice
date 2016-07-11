eb9aa488f94c41bcd753de6ec008aebac8885956
Xiaoying <yqliving@gmail.com>
2016-07-11 16:58:59 -0700

    [155. Min Stack][Accepted]committed by LeetCode Extension

---
+++ 155-Min-Stack/solution.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    private:
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int x) {
+	    s1.push(x);
+	    if (s2.empty() || x <= getMin())  s2.push(x);	    
+    }
+    void pop() {
+	    if (s1.top() == getMin())  s2.pop();
+	    s1.pop();
+    }
+    int top() {
+	    return s1.top();
+    }
+    int getMin() {
+	    return s2.top();
+    }
+};
+
+//use two stack, one shore all the data, the other store the minimum values.
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
