3eb646a9d07cccfad8737e3d0de01e0bec35af7c
Xiaoying <yqliving@gmail.com>
2016-07-11 16:31:00 -0700

    [144. Binary Tree Preorder Traversal][Accepted]committed by LeetCode Extension

--- 144-Binary-Tree-Preorder-Traversal/solution.cpp
+++ 144-Binary-Tree-Preorder-Traversal/solution.cpp
@@ -20,7 +20,7 @@ public:
         while(1) {
             if(root != NULL) {
                 ans.push_back(root->val);
-                if(root->right != NULL) treeStack.push_back(root->right);
+                if(root->right != NULL) treeStack.push(root->right);
                 root = root->left;
             } else {
                 if(treeStack.size() == 0) break;
@@ -33,7 +33,15 @@ public:
         return ans;
     }
 };
-/*
+//stack<TreeNode*> push
+//vector<TreeNode*> push_back
+/*向量容器(vector)是一种顺序容器，是一块连续分配的内存，支持随机访问，从数据安排的角度看，和数组极其相似，数组跟vector的区别在于：数组是静态分配空间，一旦分配了空间的大小，就不可以再改变了，例如,int a[6];而vector是动态分配内存，随着元素的不断插入，它会按照自身的一套机制不断扩充自身的容量，vector容器的容量增长是按照容器现在容量的一倍进行增长。
+
+begin() end() 返回vector头 尾 元素对应的地址
+front() back() 返回vector头 尾 元素
+
+
+
 //recursive
 class Solution {
 public:
