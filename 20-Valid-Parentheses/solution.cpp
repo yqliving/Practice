8e445473dc567bcd665526aa895a89cfeffd3649
Xiaoying <yqliving@gmail.com>
2016-07-11 14:19:40 -0700

    [20. Valid Parentheses][Accepted]committed by LeetCode Extension

--- 20-Valid-Parentheses/solution.cpp
+++ 20-Valid-Parentheses/solution.cpp
@@ -16,6 +16,25 @@ class Solution {
 }
 };
 
+/*
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto ch : s){
+            if(ch == '(' || ch == '[' || ch == '{') st.push(ch);
+            else if(st.empty()) return false;
+            else if(ch == ')' && st.top() == '(') st.pop();
+            else if(ch == ']' && st.top() == '[') st.pop();
+            else if(ch == '}' && st.top() == '{') st.pop();
+            else break;
+        }
+        return st.empty();
+    }
+};
+
+
+
 
 /*class Solution {
 public:
