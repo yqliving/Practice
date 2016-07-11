a9535d67c90374511a9451c8e27a911b5f12d594
Xiaoying <yqliving@gmail.com>
2016-07-11 14:18:28 -0700

    [20. Valid Parentheses][Accepted]committed by LeetCode Extension

--- 20-Valid-Parentheses/solution.cpp
+++ 20-Valid-Parentheses/solution.cpp
@@ -1,20 +1,19 @@
 class Solution {
     public:
-    bool isValid (string s) {
+    bool isValid(string s) {
     stack<char> temp;
     map<char, char> m = { {']','['},{')','('},{'}','{'} };
     for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '[' || s[i] == '{' || s[i] == '(') 
-        temp.push(s[i]);                      
-        //  class std::stack<char>’ has no member named ‘push_back’
-        
-        else if (s[i] == ']' || s[i] == '}' || s[i] == ')') {
-            if (temp.empty() || temp.top != m[s[i]]) return false;
+        if (s[i] == '[' || s[i] == '(' || s[i] == '{')
+            temp.push(s[i]);
+        else if (s[i] == ']' || s[i] == ')' || s[i] == '}')
+        {
+            if (temp.empty()||temp.top() != m[s[i]]) return false;
             else temp.pop();
         }
     }
-    return temp.empty;
-    }
+    return temp.empty();
+}
 };
 
 
