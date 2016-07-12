fd0379058b69d81a462ffd8f238c286402306f49
Xiaoying <yqliving@gmail.com>
2016-07-11 18:08:47 -0700

    [341. Flatten Nested List Iterator][Accepted]committed by LeetCode Extension

--- 341-Flatten-Nested-List-Iterator/solution.cpp
+++ 341-Flatten-Nested-List-Iterator/solution.cpp
@@ -15,48 +15,7 @@
  *     const vector<NestedInteger> &getList() const;
  * };
  */
-class NestedIterator {
-private:
-    stack<NestedInteger> nodes;
-    
-public:
-    NestedIterator(vector<NestedInteger> &nestedList) {
-        int size = nestedList.size();
-        for(int i = size - 1; i >= 0; --i) {
-            nodes.push(nestedList[i]);
-        }
-    }
 
-int next() {
-    int result = nodes.top().getInteger();
-    nodes.pop();
-    return result;
-}
-
-bool hasNext() {
-    while(!nodes.empty()) {
-        NestedInteger curr = nodes.top();
-        if(curr.isInteger()) {
-            return true;
-        }
-        
-        nodes.pop();
-        vector<NestedInteger>& adjs = curr.getList();
-        int size = adjs.size();
-        for(int i = size - 1; i >= 0; --i) {
-            nodes.push(adjs[i]);
-        }
-    }
-    
-    return false;
-    }
-};
-//The same idea as a DFS, the only tricky part probably is you have to find a value node to claim there is next. And to do that, you have to look through all the nodes in the worst case in the entire graph. So you just keep going until you find a value node; if you can't, there is no next.
-
-
-
-
-/*
 class NestedIterator {
 public:
     stack<pair<vector<NestedInteger>*, int> > s;  
@@ -99,3 +58,42 @@ public:
  * NestedIterator i(nestedList);
  * while (i.hasNext()) cout << i.next();
  */
+
+/*
+class NestedIterator {
+private:
+    stack<NestedInteger> nodes;
+    
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        int size = nestedList.size();
+        for(int i = size - 1; i >= 0; --i) {
+            nodes.push(nestedList[i]);
+        }
+    }
+
+int next() {
+    int result = nodes.top().getInteger();
+    nodes.pop();
+    return result;
+}
+
+bool hasNext() {
+    while(!nodes.empty()) {
+        NestedInteger curr = nodes.top();
+        if(curr.isInteger()) {
+            return true;
+        }
+        
+        nodes.pop();
+        vector<NestedInteger>& adjs = curr.getList();
+        int size = adjs.size();
+        for(int i = size - 1; i >= 0; --i) {
+            nodes.push(adjs[i]);
+        }
+    }
+    
+    return false;
+    }
+};
+//The same idea as a DFS, the only tricky part probably is you have to find a value node to claim there is next. And to do that, you have to look through all the nodes in the worst case in the entire graph. So you just keep going until you find a value node; if you can't, there is no next.
