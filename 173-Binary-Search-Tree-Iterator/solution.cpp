90bac86e3b19e0361fab702a07a5ca89da1b10cd
Xiaoying <yqliving@gmail.com>
2016-07-11 17:11:22 -0700

    [173. Binary Search Tree Iterator][Accepted]committed by LeetCode Extension

--- 173-Binary-Search-Tree-Iterator/solution.cpp
+++ 173-Binary-Search-Tree-Iterator/solution.cpp
@@ -8,42 +8,32 @@
  * };
  */
 class BSTIterator {
-private:
-    stack<TreeNode*> st;
+    stack<TreeNode*> s;
 public:
     BSTIterator(TreeNode *root) {
-        find_left(root);
+        while(root) {
+            s.push(root);
+            root =  root->left;
+        }
     }
 
     /** @return whether we have a next smallest number */
     bool hasNext() {
-        if (st.empty())
-            return false;
-        return true;
+        return !s.empty();
     }
 
     /** @return the next smallest number */
     int next() {
-        TreeNode* top = st.top();
-        st.pop();
-        if (top->right != NULL)
-            find_left(top->right);
-            
-        return top->val;
-    }
-    
-    /** put all the left child() of root */
-    void find_left(TreeNode* root)
-    {
-        TreeNode* p = root;
-        while (p != NULL)
-        {
-            st.push(p);
-            p = p->left;
+        TreeNode* n = s.top();
+        s.pop();
+        TreeNode* r = n->right;
+        while(r) {
+            s.push(r);
+            r = r->left;
         }
+        return n->val;
     }
 };
-
 /**
  * Your BSTIterator will be called like this:
  * BSTIterator i = BSTIterator(root);
