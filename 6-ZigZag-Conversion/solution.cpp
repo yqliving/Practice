551b9365ca1621dfbc6d6b6dc73340c65516593d
Xiaoying <yqliving@gmail.com>
2016-07-13 14:16:57 -0700

    [6. ZigZag Conversion][Accepted]committed by LeetCode Extension

--- 6-ZigZag-Conversion/solution.cpp
+++ 6-ZigZag-Conversion/solution.cpp
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
     public:
     string convert (string s, int nRows) {
         if (nRows < 2) return s;
@@ -6,27 +6,10 @@
         string res;
         for (int i = 0; i < nRows; i++) {
             for (int j = 0; j + i < n; j += 2*nRows - 2) {
-                res.push_back(s[j+1]);
+                res.push_back(s[j+i]);
                 if (i == 0 || i == nRows - 1) continue;
                 if (j + 2*nRows - 2 - i < n) 
                     res.push_back(s[j + 2*nRows -2 - i]);
-            }
-        }
-        return res;
-    }
-};*/
-class Solution {
-public:
-    string convert(string s, int nRows) {
-        if(nRows <= 1) return s;
-        int n = s.size();
-        string res;
-        for(int i = 0;i < nRows; ++i){
-            for (int j = 0; j  + i < n; j += 2*nRows - 2) {
-                res.push_back(s[j+i]);
-                if (i == 0 || i == nRows - 1) continue;
-                if (j + 2*nRows - 2 - i < n)
-                    res.push_back(s[j + 2*nRows - 2 - i]);
             }
         }
         return res;
