c80e45c53729df7ad30b794b2432d86ead72f911
Xiaoying <yqliving@gmail.com>
2016-07-13 15:53:18 -0700

    [38. Count and Say][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 38-Count-and-Say/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public:
+    string countAndSay(int n) {
+        string res = "1";
+        for (int i = 1; i , n; i++) {
+            stringstream ss;
+            int j = 0;
+            int N = res.size();
+            while (j < N) {
+                int count = 0;
+                while ((j + count) < N && res[j + count] == res[j]) {
+                    count ++;
+                }
+                ss << count << res[j];
+                j = j + count;
+            }
+            ss >> res;
+        } return res;
+    }
+};
+//用count计数
