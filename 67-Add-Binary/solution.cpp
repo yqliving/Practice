bbab820ae1a2b3981875c8925191c071b1d9d9c1
Xiaoying <yqliving@gmail.com>
2016-07-13 16:22:09 -0700

    [67. Add Binary][Accepted]committed by LeetCode Extension

--- 67-Add-Binary/solution.cpp
+++ 67-Add-Binary/solution.cpp
@@ -6,7 +6,7 @@ class Solution {
         string s = "";
         int carry = 0;
         int i = a.size() - 1, j = b.size() - 1;
-        while (i >= 0 || j >=0 || c == 1) {
+        while (i >= 0 || j >=0 || carry == 1) {
             carry += i >= 0 ? a[i--] - '0' : 0;
             carry += j >= 0 ? b[j--] - '0' : 0;
             s = char(carry % 2 + '0') + s;
