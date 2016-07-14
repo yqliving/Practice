10c38502fe03247cfa216df9ae7f5c58d8d9462a
Xiaoying <yqliving@gmail.com>
2016-07-14 13:48:34 -0700

    [345. Reverse Vowels of a String][Accepted]committed by LeetCode Extension

---
+++ 345-Reverse-Vowels-of-a-String/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        auto p1 = s.begin(), p2 = s.end() - 1;
+        string vowels = "aeiouAEIOU";
+        while(p1 < p2) {
+            while((vowels.find(*p1) == string::npos) && (p1 < p2)) p1++;
+            while((vowels.find(*p2) == string::npos) && (p1 < p2)) p2--;
+            if(p1 < p2) swap(*p1, *p2);
+            p1++;
+            p2--;
+        }
+        return s;
+    }
+};
+//当 str.find("哦")==string::npos时则说明字符串str中不存在“哦”这个字符
