22e2a74f0b51dbe32ce5afa99b00af834ed1e08f
Xiaoying <yqliving@gmail.com>
2016-07-14 12:16:37 -0700

    [93. Restore IP Addresses][Compile Error]committed by LeetCode Extension

---
+++ 93-Restore-IP-Addresses/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<vector<string>> cur(4), prev(4);
+        cur[0].push_back(s.front());
+        for (int i = 1; i < s.size(); ++i) {
+            swap(cur, prev);
+            for (int p = 0; p < 4; ++p) {
+                cur[p].clear();
+                if (p > 0) {
+                    for (auto& str : prev[p - 1]) {
+                        cur[p].push_back(str + "." + s[i]);
+                    }
+                }
+                for (auto str : prev[p]) {
+                    str += s[i];
+                    auto pos = str.rfind('.');
+                    int last = stoi(str.substr(pos + 1));
+                    if (str[pos + 1] != '0' && last <= 255)
+                        cur[p].push_back(str);
+                }                
+            }
+        }
+        return cur[3];
+    }
+};
