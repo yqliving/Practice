1cf9d186e6f1ec5506b5a2a05efeb8aa1de55730
Xiaoying <yqliving@gmail.com>
2016-07-14 12:31:59 -0700

    [93. Restore IP Addresses][Compile Error]committed by LeetCode Extension

--- 93-Restore-IP-Addresses/solution.cpp
+++ 93-Restore-IP-Addresses/solution.cpp
@@ -1,15 +1,43 @@
 class Solution {
     public:
-      vector<string> restoreIpAddresses(const string& s) {
-vector<string> result; vector<string> ip; //        
-dfs(s, ip, result, 0);
-return result;
-}
-void dfs(string s, vector<string>& ip, vector<string> &result,
-              size_t start) {
-if (ip.size() == 4 && start == s.size()) { //       解 
-result.push_back(ip[0] + '.' + ip[1] + '.' + ip[2] + '.' + ip[3]); 
-return;
+    vector<string> restoreIpAddress(const string s) {
+        vector<string> res, ip;
+        dfs(s, ip, res, 0);
+        return res;
+    }
+    
+    void dfs(string s, vector<string>& ip, vector<string>& res, int start) {
+        if (ip.size() == 4 && start == s.size()) {
+            res.push_back(ip[0] + '.' + ip[1] + '.' + ip[2] + '.' + ip[3]);
+            return
+        }
+        if (s.size() - start > (4 - ip.size()) * 3) return;
+        if (s.size() - start < (4 - ip.size())) return;
+        
+        int num = 0;
+        for (int i = start; i < start + 3; i++) {
+            num = num * 10 + (s[i] - '0');
+            if (num < 0 || num > 255) continue;
+            ip.push_back(s.substr(start, i - start + 1));
+            dfs(s, ip, result, i + 1);
+            ip.pop_back();
+            if (num == 0) break;
+        }
+    }
+};
+/*
+class Solution {
+    public:
+    vector<string> restoreIpAddresses(const string& s) {
+    vector<string> result; vector<string> ip; //        
+    dfs(s, ip, result, 0);
+    return result;
+    }
+    
+     void dfs(string s, vector<string>& ip, vector<string> &result, size_t start) {
+     if (ip.size() == 4 && start == s.size()) { //       解 
+         result.push_back(ip[0] + '.' + ip[1] + '.' + ip[2] + '.' + ip[3]); 
+         return;
 }
 if (s.size() - start > (4 - ip.size()) * 3) return; //   
 if (s.size() - start < (4 - ip.size())) return; //   
