11954c05662c9789d9a13a93b38ddf9a75b10316
Xiaoying <yqliving@gmail.com>
2016-07-15 15:39:23 -0700

    [53. Maximum Subarray][Compile Error]committed by LeetCode Extension

---
+++ 53-Maximum-Subarray/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    public:
+    int maxSubArray(vector<int>& nums) {
+        int len = nums.size();
+        if (len == 0) return 0;
+        if (l == 0) return 0;
+        int maxn, temp;
+        maxn = temp = nums[0];
+        for (int i = 1; i < len; i++) {
+            temp = max(nums[i], temp + nums[i]);
+            maxn = max(maxn, temp);
+        }
+        return maxn;
+    }
+};
+
+/*first time
+
+/*class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int l = nums.size();
+        int tmp ,maxn;
+        if (l == 0) return 0;
+        tmp = maxn = nums[0];
+        for (int i = 1;i < l;++i) {
+            tmp = max(nums[i],tmp+nums[i]);
+            maxn = max(tmp,maxn);
+        }
+        return maxn;
+    }
+};
+/*O(1)space,O(N)time
