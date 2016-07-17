3af2f0dc3c5788059799dc99ae0e87b9b6d1ce66
Xiaoying <yqliving@gmail.com>
2016-07-17 12:36:02 -0700

    [79. Word Search][Compile Error]committed by LeetCode Extension

--- 79-Word-Search/solution.cpp
+++ 79-Word-Search/solution.cpp
@@ -1,23 +1,27 @@
 class Solution {
     public:
     bool exist(vector<vector<char>>& board, string word) {
-        int lenR = board.size(), lenC = board[0].size();
-        if (lenR == 0 || lenC == 0) return false;
+        int row = board.size();
+        if (row == 0) return false;
+        int col = board[0].szie();
+        if (col == 0) return false;
         
-        for (int i = 0; i < lenR; i++) {
-            for (int j = 0; j < lenC; j++) {
-                if (findWord(i, j, 0, word, board)) return true;
-            }
-            return false;
-        }
+        for(int i = 0; i < row; i++)
+            for(int j = 0; j < col; j++)
+                if(findWord(i, j, 0, word, board)) return true;
+        return false;
     }
-    
-    private: 
-    int lenR, lenC;
-    bool findWord(int i, int j, int idx, string& word, vector<vector<char>>& board) {
-        if (idx == word.size()) return true;
-        if(i < 0 || j < 0 || i >= lenR || j >= lenC) return false;
+
+private:
+    int row;
+    int col;
+
+    bool findWord(int i, int j, int idx, string& word, vector<vector<char>>& board){
+        if(idx == word.size()) return true;
+
+        if(i < 0 || j < 0 || i >= row || j >= col) return false;
         if(word[idx] != board[i][j]) return false;
+
         board[i][j] = '\0';
         if (findWord(i-1, j, idx+1, word, board) || 
             findWord(i+1, j, idx+1, word, board) || 
