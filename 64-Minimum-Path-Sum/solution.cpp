a50009f61c6d630731b013d1e4421277ce47826a
Xiaoying <yqliving@gmail.com>
2016-07-17 10:34:54 -0700

    [64. Minimum Path Sum][Accepted]committed by LeetCode Extension

--- 64-Minimum-Path-Sum/solution.cpp
+++ 64-Minimum-Path-Sum/solution.cpp
@@ -1,30 +1,27 @@
-/*class Solution {
-public:
-int minPathSum(vector<vector<int> > &grid) {
-    if (grid.size()<=0){
-        return 0;
-    }
-    int i, j;
-    for(i=0; i<grid.size(); i++){
-        for(j=0; j<grid[i].size(); j++){
-            int top = i-1<0 ? INT_MAX : grid[i-1][j] ;
-            int left = j-1<0 ? INT_MAX : grid[i][j-1];
-            if (top==INT_MAX && left==INT_MAX){
-                continue;
+class Solution {
+    public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if (grid.empty()) return 0;
+        int lenR = grid.size();
+        int lenC = grid[0].size();
+        
+        for (int i = 0; i <lenR; i ++) {
+            for (int j = 0; j < lenC; j++) {
+                int top  = (i - 1) < 0 ? INT_MAX : grid[i-1][j];
+                int left = (j - 1) < 0 ? INT_MAX : grid[i][j-1];
+                if (top == INT_MAX && left == INT_MAX) continue;
+                grid[i][j] += top < left ? top : left;
             }
-            grid[i][j] += (top < left? top: left);
-
         }
+        return grid[lenR - 1][lenC - 1];
     }
+};
 
-    return grid[grid.size()-1][grid[0].size()-1];
-}
-};*/
 /*2-d DP: Time ~ O(N^2), Space ~ O(M*N)
 Let d(i,  j) be the min path sum from grid[0,  0] (top left) to grid[i, j].
 d(i, j) = min{d(i - 1, j), d(i, j - 1)} + grid[i, j];
 Return d(M - 1, N - 1).
-Use top-down approach.*/
+Use top-down approach.
 
 class Solution {
 public:
