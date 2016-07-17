0599ef2812a22e1c2c67cc911cd06e445d32ea71
Xiaoying <yqliving@gmail.com>
2016-07-17 14:40:43 -0700

    [105. Construct Binary Tree from Preorder and Inorder Traversal][Accepted]committed by LeetCode Extension

--- 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.cpp
+++ 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.cpp
@@ -7,25 +7,37 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
-    public:
+
+public:
+    /* from Preorder and Inorder Traversal */
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return Build(preorder, 0, inorder, 0);
+
+        return helper(preorder,0,preorder.size(),inorder,0,inorder.size());
+    
     }
-    TreeNode* Build(vector<int>& preorder, int i, vector<int>& inorder, int ii) {
-        int j = preorder.size();
-        int jj = inorder.size();
-        if (i >= j || ii >= j) return NULL;
-        
-        int min = preorder[i];
+
+    TreeNode* helper(vector<int>& preorder,int i,int j,vector<int>& inorder,int ii,int jj)
+    {
+        // tree        8 4 5 3 7 3
+        // preorder    8 [4 3 3 7] [5]
+        // inorder     [3 3 4 7] 8 [5]
+
+        // 每次从 preorder 头部取一个值 mid，作为树的根节点
+        // 检查 mid 在 inorder 中 的位置，则 mid 前面部分将作为 树的左子树，右部分作为树的右子树
+
+        if(i >= j || ii >= j)
+            return NULL;
+
+        int mid = preorder[i];
         auto f = find(inorder.begin() + ii,inorder.begin() + jj,mid);
 
         int dis = f - inorder.begin() - ii;
 
         TreeNode* root = new TreeNode(mid);
-        root -> left = Build(preorder,i + 1,inorder,ii);
-        root -> right = Build(preorder,i + 1 + dis,inorder,ii + dis + 1);
+        root -> left = helper(preorder,i + 1,i + 1 + dis,inorder,ii,ii + dis);
+        root -> right = helper(preorder,i + 1 + dis,j,inorder,ii + dis + 1,jj);
         return root;
-        
-        }
+    }
 };
