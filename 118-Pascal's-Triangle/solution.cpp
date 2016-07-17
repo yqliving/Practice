25d5e4ada558242c912cd1ea66d15d9d6f641534
Xiaoying <yqliving@gmail.com>
2016-07-17 15:37:40 -0700

    [118. Pascal's Triangle][Accepted]committed by LeetCode Extension

--- 118-Pascal's-Triangle/solution.cpp
+++ 118-Pascal's-Triangle/solution.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        for (int i = 1; i <= numRows; i++) {
+            for (int j = i - 1; j >= 2; j--) {
+                temp[j - 1] += temp[j - 2];
+            }
+            temp.push_back(1);
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
+
+
+
+/*class Solution {
+public:
     vector<vector<int> > generate(int numRows) {
         vector<vector<int>> res(numRows);      ///initial 2D  如果是一维的，可以直接res（num， 1）
         if (numRows < 1)
