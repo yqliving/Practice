efbed467df1b5e4058d6825628251a5b16378710
Xiaoying <yqliving@gmail.com>
2016-07-17 11:10:51 -0700

    [73. Set Matrix Zeroes][Compile Error]committed by LeetCode Extension

--- 73-Set-Matrix-Zeroes/solution.cpp
+++ 73-Set-Matrix-Zeroes/solution.cpp
@@ -5,30 +5,31 @@ zero rows (except the first row) according to the first column;
 zero columns (except the first column) according to the first row;
 zero first row and first column if needed. */
 
-
 class Solution {
-public:
-    void setZeroes(vector<vector<int>>& matrix) {
+    public:
+    void setZeroes{vector<vector<int>>& matrix} {
         bool row = false, col = false;
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[0].size(); j++){
-                if(matrix[i][j] == 0) {
-                    if(i == 0) row = true;
-                    if(j == 0) col = true;
+        int lenR = matrix.size(), lenC = matrix[0].size();
+
+        for (int i = 0; i < lenR; i++) {
+            for (int j = 0; j < lenC; j ++) {
+                if (maxtrix[i][j] == 0) {
+                    if (i == 0) row = true;
+                    if (j == 0) col = true;
                     matrix[0][j] = matrix[i][0] = 0;
                 }
             }
         }
-        for(int i = 1; i < matrix.size(); i++){
-            for(int j = 1; j < matrix[0].size(); j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+        for (int i = 1; i < lenR; i++) {
+            for (int j = 1; j < lenC; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
         }
-        if(col){
-            for(int i = 0; i < matrix.size(); i++) matrix[i][0] = 0;
+        if (row) {
+            for (j = 0; j < lenC; j++) matrix[0][j] = 0;
         }
-        if(row){
-            for(int j = 0; j < matrix[0].size(); j++) matrix[0][j] = 0;
+        if (col) {
+            for (i = 0; i < lenR; i++) matrix[i][0] = 0;
         }
     }
 };
