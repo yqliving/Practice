82b65cdf44a36fccf56c18190284e174f73973d2
Xiaoying <yqliving@gmail.com>
2016-07-18 12:10:20 -0700

    [217. Contains Duplicate][Compile Error]committed by LeetCode Extension

--- 217-Contains-Duplicate/solution.cpp
+++ 217-Contains-Duplicate/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
     public:
     bool containDuplicate(vector<int>& nums) {
-        vector<int>& temp;
+        vector<int> temp;
         for (int i = 0; i < nums.szie(); i++) {
             if (temp.count(nums[i])) return true;
             temp[i] = nums[i];
