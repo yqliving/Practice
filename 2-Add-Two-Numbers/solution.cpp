2538bbb493ac068dc606065da69b069ddb5b5d53
Xiaoying <yqliving@gmail.com>
2016-07-18 15:15:35 -0700

    [2. Add Two Numbers][Compile Error]committed by LeetCode Extension

--- 2-Add-Two-Numbers/solution.cpp
+++ 2-Add-Two-Numbers/solution.cpp
@@ -7,29 +7,25 @@
  * };
  */
 class Solution {
-public:
- ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    ListNode * res=NULL, *next, *temp;
-    int carry=0, sum=0;
-
-    while(l1 || l2)
-    {
-        sum = carry + (l1?l1->val:0)+(l2?l2->val:0);
-        carry = (sum >= 10) ? 1 : 0;
-        sum = sum % 10;
-        temp = new ListNode(sum);
-        if(!res) res=temp;
-        else next->next=temp;  //把node链接
-        next=temp;
-
-
-        if(l1)l1=l1->next;
-        if(l2)l2=l2->next;
+    public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* res = NULL, * next, * temp;
+        int carry = 0, sum = 0;
+        
+        while(l1 || l2) {
+            sum = carry + (l1 ? l1->val : 0) + (l2 ? l2->value : 0);
+            carry = sum > 10 ? 1 : 0;
+            sum = sum % 10;
+            temp = new ListNode(sum);
+            if (!res) res = temp;
+            else next->next = temp;
+            next = temp;
+        }
+        if (carry > 0) temp->next = new ListNode(carry);
+        return res;
     }
-    if(carry>0)temp->next=new ListNode(carry);
-    return res;
 }
-};
+
 
 // Java
 /*
