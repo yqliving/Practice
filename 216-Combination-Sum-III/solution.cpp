d0f780618fab415ecb3cd8c23a1b377636dd9577
Xiaoying <yqliving@gmail.com>
2016-07-18 12:01:10 -0700

    [216. Combination Sum III][Compile Error]committed by LeetCode Extension

---
+++ 216-Combination-Sum-III/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+    vector<vector<int>> res;
+    vector<int> temp;
+    dfs(res, temp, 1, k, n);
+    return res;
+    }
+    
+    void dfs(vector<vector<int>>& res, vector<int>& temp, int start, int k, int n) {
+        if (k == 0 || n == 0) {
+            res.push_back(temp);
+            return;
+        }
+        if (start > 9 || start > n) return;
+        temp.push_back(start);
+        dfs (res, temp, start + 1, k - 1, n - start);
+        temp.pop_back(start);
+        dfs (res, temp, start, k, n);
+    }
+};
