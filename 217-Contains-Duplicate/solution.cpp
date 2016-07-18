1db5cc6bee3474494d5910032a8243c96d34cc6c
Xiaoying <yqliving@gmail.com>
2016-07-18 12:10:08 -0700

    [217. Contains Duplicate][Compile Error]committed by LeetCode Extension

---
+++ 217-Contains-Duplicate/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+    bool containDuplicate(vector<int>& nums) {
+        vector<int>& temp;
+        for (int i = 0; i < nums.szie(); i++) {
+            if (temp.count(nums[i])) return true;
+            temp[i] = nums[i];
+        }
+        return false;
+    } 
+};
+/*class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.count(nums[i])) return true;
+            mp[nums[i]] = i;
+            
+        }
+        return false;
+        
+    }
+};
