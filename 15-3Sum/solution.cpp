6a67a6a309efdfe11257be3847324a6ef68ab16f
Xiaoying <yqliving@gmail.com>
2016-07-19 11:27:14 -0700

    [15. 3Sum][Compile Error]committed by LeetCode Extension

--- 15-3Sum/solution.cpp
+++ 15-3Sum/solution.cpp
@@ -1,31 +1,29 @@
-
 class Solution {
-public:
+    public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        if(nums.size() < 3) return ans;
-        
+        vector<vector<int>> res;
+        int len = nums.size();
+        if(len < 3) return res;
         sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size() - 2; i ++) {
-            // in case of duplicate solutions
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+        for (int i = 0; i < len; i++) {
+            if (i > 0 && nums[i] = nums[i-1]) continue;
             int a = nums[i];
-            int lf = i + 1;
-            int rt = nums.size() - 1;
-            while(lf < rt) {
-                int b = nums[lf];
-                int c = nums[rt];
-                if(0 == a + b + c) {
-                    ans.push_back(vector<int> {a, b, c});
-                    // in case of duplicate solutions
-                    while(lf < nums.size() && nums[lf] == b) lf ++;
-                    // in case of duplicate solutions
-                    while(rt >= 0 && nums[rt] == c) rt --;
+            int left = i + 1;
+            int right = len - 1;
+            while(left < right) {
+                int b = nums[left];
+                int c = nums[right];
+                if (a + b + c == 0) {
+                    res.push_back(vector<int> {a, b, c});
+                    
+                    while (left < len && nums[left] == b) left++;
+                    while (right > 0 && nums[right] == c) right --;
                 }
-                else if(a + b + c > 0) rt --;
-                else lf ++;
+                else if (a + b + c > 0) right --;
+                else left --;
             }
         }
-        return ans;
+        return res;
+        
     }
 };
