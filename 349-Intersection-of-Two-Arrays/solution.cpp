ccde83cc17e02bea850dc37597d86e7d4410507c
Xiaoying <yqliving@gmail.com>
2016-07-19 12:20:46 -0700

    [349. Intersection of Two Arrays][Accepted]committed by LeetCode Extension

---
+++ 349-Intersection-of-Two-Arrays/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> m(nums1.begin(), nums1.end());
+        vector<int> res;
+        for (auto a : nums2)
+            if (m.count(a)) {
+                res.push_back(a);
+                m.erase(a);
+            }
+        return res;
+    }
+};
+
+//two pointer
+/*
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int n1 = (int)nums1.size(), n2 = (int)nums2.size();
+        int i1 = 0, i2 = 0;
+        vector<int> res;
+        while(i1 < n1 && i2 < n2){
+            if(nums1[i1] == nums2[i2]) {
+                res.push_back(nums1[i1]);
+                i1++;
+                i2++;
+            }
+            else if(nums1[i1] > nums2[i2]){
+                i2++;
+            }
+            else{
+                i1++;
+            }
+            while(i1 > 0 && i1 < n1 && nums1[i1] == nums1[i1 - 1]) i1++;
+            while(i2 > 0 && i2 < n2 && nums2[i2] == nums2[i2 - 1]) i2++;
+        }
+        return res;
+    }
+};
