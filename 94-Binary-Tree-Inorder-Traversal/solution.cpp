71a79b8020e3a1fe2e6717465694268e686e4536
Xiaoying <yqliving@gmail.com>
2016-07-19 21:18:40 -0700

    [94. Binary Tree Inorder Traversal][Accepted]committed by LeetCode Extension

--- 94-Binary-Tree-Inorder-Traversal/solution.cpp
+++ 94-Binary-Tree-Inorder-Traversal/solution.cpp
@@ -59,3 +59,33 @@ class Solution {
 
 //stack, 先排左子树，然后反过来输出，在看右子树
 //中序遍历，顺时针
+
+/*Comparing with the preorder traversal, the difference is you can't just push the node of the value when popping it from the stack. You can do that only when 1) the node is visited the second time, implying that its left subtree has been processed, or 2) when the node has no left subtree.
+
+Using a Hash to mark if the node is visited the first time making the solution straightforward. */
+/*
+   class Solution {
+         public:
+        vector<int> inorderTraversal(TreeNode* root) {
+            vector<int> r;
+            if (!root) return r;
+            stack<TreeNode *> nodes;
+            nodes.push(root);
+            unordered_map<TreeNode* ,bool> visited;
+            while(!nodes.empty()) {
+                TreeNode *c = nodes.top();
+                if (!visited[c] && (c->left)) {
+                    nodes.push(c->left);
+                    visited[c] = true;
+                } else {
+                    r.push_back(c->val);
+                    nodes.pop();
+                    if (c->right) {
+                        nodes.push(c->right);
+                    }
+                }
+            }
+            
+            return r;
+        }
+    };
