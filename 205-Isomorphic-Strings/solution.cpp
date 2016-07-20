afeb65a993524466b7f45b32356f5a18a9a7506c
Xiaoying <yqliving@gmail.com>
2016-07-20 11:02:48 -0700

    [205. Isomorphic Strings][Accepted]committed by LeetCode Extension

--- 205-Isomorphic-Strings/solution.cpp
+++ 205-Isomorphic-Strings/solution.cpp
@@ -4,6 +4,7 @@ public:
         //int m1[256] = {0}, m2[256] = {0}, n = s.size();
         int n = s.size();
         unordered_map<char, int> m1, m2;
+        
         for (int i = 0; i < n; ++i) {
             if (m1[s[i]] != m2[t[i]]) return false;
             m1[s[i]] = i + 1;
