5b2dc4ae5b522627eb9093d07d4fe27ac54aa6f9
Xiaoying <yqliving@gmail.com>
2016-07-19 21:08:57 -0700

    [49. Group Anagrams][Accepted]committed by LeetCode Extension

--- 49-Group-Anagrams/solution.cpp
+++ 49-Group-Anagrams/solution.cpp
@@ -1,22 +1,22 @@
 class Solution {
-    public:
-    vector<vector<string>> groupAnagrams(vector<string> & strs) {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map <string, vector<string>> count;
         int i = 0;
         for (auto s : strs) {
             sort(s.begin(), s.end());
-            count[s].push_back(strs[i++]);
+            count[s].push_back(strs[i]);
+            i++;
         }
+        
         vector<vector<string>> res;
         for (auto c : count) {
-            sort(c.second.begin(), c.second.end());  // output from a-z
+            sort(c.second.begin(), c.second.end());
             res.push_back(c.second);
         }
         return res;
     }
 };
-
-
 //map like hash table
 // s extract an element in strs every
 // count first [abc] second:"abc","cab"...
