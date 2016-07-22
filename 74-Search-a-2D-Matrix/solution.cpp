97461ecda8d6ce8f53f46a59b7436f329a3ce3fb
Xiaoying <yqliving@gmail.com>
2016-07-22 15:52:11 -0700

    [74. Search a 2D Matrix][Accepted]committed by LeetCode Extension

--- 74-Search-a-2D-Matrix/solution.cpp
+++ 74-Search-a-2D-Matrix/solution.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+   // O(m+n) solution
+
+ bool searchMatrix(vector<vector<int>>& matrix, int target) {
+ int i = 0;
+ int j = matrix[0].size() - 1;
+ 
+ while(i < matrix.size() && j >= 0) {
+     if(matrix[i][j] == target)
+         return true;
+     
+     if(matrix[i][j] < target)
+         i++;
+     else
+         j--;
+ }
+ 
+ return false;
+ }
+
+};
+/*class Solution {
+public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row = matrix.size();
         int col = matrix[0].size();
