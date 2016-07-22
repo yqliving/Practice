b288dd8a1ff70763147566a1b7e214038b9a6584
Xiaoying <yqliving@gmail.com>
2016-07-22 15:23:07 -0700

    [33. Search in Rotated Sorted Array][Accepted]committed by LeetCode Extension

--- 33-Search-in-Rotated-Sorted-Array/solution.cpp
+++ 33-Search-in-Rotated-Sorted-Array/solution.cpp
@@ -7,11 +7,13 @@ public:
             int mid = l + (r - l) / 2;
             
             if(nums[mid] == target) return mid;
-            if(nums[mid] > nums[r]){
-                if(target > nums[mid] || target <= nums[r]) l = mid + 1;   // condition for pick right side
+            if(nums[mid] > nums[r]){// two situation
+                if(target > nums[mid] || target <= nums[r]) l = mid + 1;   
+                // condition for pick right side
                 else r = mid - 1;    // else, pick left side
             }else{
-                if(target <= nums[r] && target > nums[mid]) l = mid + 1;  // condition for pick right side
+                if(target <= nums[r] && target > nums[mid]) l = mid + 1;  
+                // condition for pick right side
                 else r = mid - 1;     // else, pick left side
             } 
         }
@@ -19,3 +21,4 @@ public:
         return -1;
     }
 };
+//because rotated, nums[mid] > nums[r]   1 2 4 5 6 7 9 might become 4 5 6 7 9 1 2
