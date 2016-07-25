4e04694be409e5db44140f51962ab32b80d206eb
Xiaoying <yqliving@gmail.com>
2016-07-25 16:10:34 -0700

    [179. Largest Number][Accepted]committed by LeetCode Extension

--- 179-Largest-Number/solution.cpp
+++ 179-Largest-Number/solution.cpp
@@ -1,4 +1,30 @@
-class Solution {
+class Solution { 
+public:
+string largestNumber(vector<int> &num) {
+    string result;
+    vector<string> str;
+    for (auto n : num) {
+        str.push_back(to_string(n));
+    }
+    sort(str.begin(), str.end(), compareNum);
+    for (auto s: str) {
+        result += s;
+    }
+    
+    int pos = 0;
+    while (result[pos] == '0' && pos + 1 < result.size()) pos++; 
+    return result.substr(pos);
+} 
+private:
+static bool compareNum(string a, string b) {
+    return a + b > b + a;
+}
+};
+
+
+
+
+/*class Solution {
 public:
     string largestNumber(vector<int> &num) {
         sort(num.begin(), num.end(), [](int a, int b){
