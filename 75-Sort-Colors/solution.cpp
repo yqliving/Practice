4f10ff01c34c87fa31a2c69647503fb3522a52ee
Xiaoying <yqliving@gmail.com>
2016-07-25 15:45:31 -0700

    [75. Sort Colors][Accepted]committed by LeetCode Extension

--- 75-Sort-Colors/solution.cpp
+++ 75-Sort-Colors/solution.cpp
@@ -27,3 +27,15 @@ Use two pointers (one-pass): Time ~ O(N), Space ~ O(1)
 A[curr] == 0: swap(curr, red); curr++; red++;
 A[curr] == 1: curr++;
 A[curr] == 2: swap(curr, blue); blue--.
+
+/*
+class Solution {
+    public:
+        void sortColors(int A[], int n) {
+            int second=n-1, zero=0;
+            for (int i=0; i<=second; i++) {
+                while (A[i]==2 && i<second) swap(A[i], A[second--]);
+                while (A[i]==0 && i>zero) swap(A[i], A[zero++]);
+            }
+        }
+    };
