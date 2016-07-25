d39c407570324761aef3115bd0cb7f4f3a5d1e9c
Xiaoying <yqliving@gmail.com>
2016-07-25 15:47:29 -0700

    [75. Sort Colors][Accepted]committed by LeetCode Extension

--- 75-Sort-Colors/solution.cpp
+++ 75-Sort-Colors/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
     public:
-        void sortColors(int A[], int n) {
-            int second=n-1, zero=0;
+        void sortColors(vector<int>& A) {
+            int second=A.size()-1, zero=0;
             for (int i=0; i<=second; i++) {
                 while (A[i]==2 && i<second) swap(A[i], A[second--]);
                 while (A[i]==0 && i>zero) swap(A[i], A[zero++]);
