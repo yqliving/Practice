9ea94e5e71533e37ba0fda2cc16244a8a7c7426c
Xiaoying <yqliving@gmail.com>
2016-07-25 15:46:18 -0700

    [75. Sort Colors][Compile Error]committed by LeetCode Extension

--- 75-Sort-Colors/solution.cpp
+++ 75-Sort-Colors/solution.cpp
@@ -1,4 +1,26 @@
 class Solution {
+    public:
+        void sortColors(int A[], int n) {
+            int second=n-1, zero=0;
+            for (int i=0; i<=second; i++) {
+                while (A[i]==2 && i<second) swap(A[i], A[second--]);
+                while (A[i]==0 && i>zero) swap(A[i], A[zero++]);
+            }
+        }
+    };
+    
+    
+
+/*swap函数，作用是交换两个元素的值
+Use two pointers (one-pass): Time ~ O(N), Space ~ O(1) 
+因为只有三种颜色，可以设 red (0) 和 blue (2) 两个 index，从两头向中间逼近。
+同时用 curr index 从头到尾扫描 array，有三种情况：
+A[curr] == 0: swap(curr, red); curr++; red++;
+A[curr] == 1: curr++;
+A[curr] == 2: swap(curr, blue); blue--.
+
+/*
+class Solution {
 public:
     void sortColors(vector<int>& A) {
      int n = A.size();
@@ -20,22 +42,3 @@ public:
         }
     }
 };   
-/*swap函数，作用是交换两个元素的值
-Use two pointers (one-pass): Time ~ O(N), Space ~ O(1) 
-因为只有三种颜色，可以设 red (0) 和 blue (2) 两个 index，从两头向中间逼近。
-同时用 curr index 从头到尾扫描 array，有三种情况：
-A[curr] == 0: swap(curr, red); curr++; red++;
-A[curr] == 1: curr++;
-A[curr] == 2: swap(curr, blue); blue--.
-
-/*
-class Solution {
-    public:
-        void sortColors(int A[], int n) {
-            int second=n-1, zero=0;
-            for (int i=0; i<=second; i++) {
-                while (A[i]==2 && i<second) swap(A[i], A[second--]);
-                while (A[i]==0 && i>zero) swap(A[i], A[zero++]);
-            }
-        }
-    };
