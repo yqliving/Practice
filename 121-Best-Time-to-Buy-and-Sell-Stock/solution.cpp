59a9a56b7fb423634e00b8c7d2295fb8170cc4b7
Xiaoying <yqliving@gmail.com>
2016-07-27 12:32:08 -0700

    [121. Best Time to Buy and Sell Stock][Runtime Error]committed by LeetCode Extension

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int curMin = prices[0];
+        int maxValue = 0;
+        
+        for (int i = 0; i < prices.size(); i++) {
+           curMin = min(curMin, prices[i]);
+           maxValue = max(maxValue, prices[i] - curMin);
+        }
+        return maxValue;
+    }
+};
+
+/*// This solution is O(1) space dynamic programming 
+    //
+    // We can make sure the max profit at least be ZERO. So,
+    //    1) we have two pointers (begin & end ) 
+    //    2) if prices[end] - prices[begin] >  0, then move the "end" pointer to next
+    //    3) if prices[end] - prices[begin] <= 0, then move the "begin" pointer to current posstion.
+    //    4) tracking the max profit
+    //
+    // Notes:
+    //    Some people think find the highest-price & lowest-price, this is wrong. 
+    //    Because the highest-price must be after lowest-price
+    //
