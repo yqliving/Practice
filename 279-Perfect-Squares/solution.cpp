df85a995adc5c3a2736908f159639c8a2fbbfe87
Xiaoying <yqliving@gmail.com>
2016-07-27 14:44:58 -0700

    [279. Perfect Squares][Runtime Error]committed by LeetCode Extension

--- 279-Perfect-Squares/solution.cpp
+++ 279-Perfect-Squares/solution.cpp
@@ -4,7 +4,7 @@ public:
         if(n <= 0){
             return 0;
         }
-        vector<int> cnt(n + 1, INT_MAX);
+        static vector<int> cnt(n + 1, INT_MAX);
         cnt[0] = 0;
         for(int i = 1; i <= n; ++i){
             for(int j = 1; j * j <= i; ++j){
@@ -12,6 +12,42 @@ public:
             }
         }
         return cnt[n];
+    }
+};
+
+//2.Static Dynamic Programming: 12ms
+/*
+class Solution 
+{
+public:
+    int numSquares(int n) 
+    {
+        if (n <= 0)
+        {
+            return 0;
+        }
+        
+        // cntPerfectSquares[i] = the least number of perfect square numbers 
+        // which sum to i. Since cntPerfectSquares is a static vector, if 
+        // cntPerfectSquares.size() > n, we have already calculated the result 
+        // during previous function calls and we can just return the result now.
+        static vector<int> cntPerfectSquares({0});
+        
+        // While cntPerfectSquares.size() <= n, we need to incrementally 
+        // calculate the next result until we get the result for n.
+        while (cntPerfectSquares.size() <= n)
+        {
+            int m = cntPerfectSquares.size();
+            int cntSquares = INT_MAX;
+            for (int i = 1; i*i <= m; i++)
+            {
+                cntSquares = min(cntSquares, cntPerfectSquares[m - i*i] + 1);
+            }
+            
+            cntPerfectSquares.push_back(cntSquares);
+        }
+        
+        return cntPerfectSquares[n];
     }
 };
 
@@ -67,41 +103,7 @@ public:
     }
 }; */
 
-/*2.Static Dynamic Programming: 12ms
 
-class Solution 
-{
-public:
-    int numSquares(int n) 
-    {
-        if (n <= 0)
-        {
-            return 0;
-        }
-        
-        // cntPerfectSquares[i] = the least number of perfect square numbers 
-        // which sum to i. Since cntPerfectSquares is a static vector, if 
-        // cntPerfectSquares.size() > n, we have already calculated the result 
-        // during previous function calls and we can just return the result now.
-        static vector<int> cntPerfectSquares({0});
-        
-        // While cntPerfectSquares.size() <= n, we need to incrementally 
-        // calculate the next result until we get the result for n.
-        while (cntPerfectSquares.size() <= n)
-        {
-            int m = cntPerfectSquares.size();
-            int cntSquares = INT_MAX;
-            for (int i = 1; i*i <= m; i++)
-            {
-                cntSquares = min(cntSquares, cntPerfectSquares[m - i*i] + 1);
-            }
-            
-            cntPerfectSquares.push_back(cntSquares);
-        }
-        
-        return cntPerfectSquares[n];
-    }
-};
 /*
 3.Mathematical Solution: 4ms
 
