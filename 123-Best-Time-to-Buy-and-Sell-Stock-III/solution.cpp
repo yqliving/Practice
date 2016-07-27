f26e898e69cda5a903debc26d7d20d95627ee733
Xiaoying <yqliving@gmail.com>
2016-07-27 12:47:16 -0700

    [123. Best Time to Buy and Sell Stock III][Runtime Error]committed by LeetCode Extension

--- 123-Best-Time-to-Buy-and-Sell-Stock-III/solution.cpp
+++ 123-Best-Time-to-Buy-and-Sell-Stock-III/solution.cpp
@@ -39,7 +39,7 @@ states[][2]: two buys, one sell
 states[][3]: two buy, two sells
 The states transistions occurs when buy/sell operations are executed. For example, state[][0] can move to state[][1] via one sell operation.
 */
-class Solution {
+/*class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int states[2][4] = {INT_MIN, 0, INT_MIN, 0}; // 0: 1 buy, 1: one buy/sell, 2: 2 buys/1 sell, 3, 2 buys/sells
