e19f34202cd7cfa8a8c0abae9aa2597031a0a8de
Xiaoying <yqliving@gmail.com>
2016-07-27 15:47:08 -0700

    [309. Best Time to Buy and Sell Stock with Cooldown][Accepted]committed by LeetCode Extension

--- 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.cpp
+++ 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.cpp
@@ -12,53 +12,32 @@ public:
     }
 };
 
+/* 此题需要维护三个一维数组buy, sell，和rest。其中：
 
-/*class Solution {
-public:
-int maxProfit(vector<int>& p) 
-{
-    if (p.size() < 2)
-        return 0;
-    int i, sz = p.size();
-    int ret = 0;
-    vector<int> buy(sz, 0);
-    vector<int> sell(sz, 0);
-    buy[0] = -p[0];
-    for (i = 1; i < sz; ++i)
-    {
-        sell[i] = max(buy[i - 1] + p[i], sell[i - 1] - p[i - 1] + p[i]);
-        if (ret < sell[i]) //record the max sell[i]
-            ret = sell[i];
-        if (1 == i)
-            buy[i] = buy[0] + p[0] - p[1];
-        else
-            buy[i] = max(sell[i - 2] - p[i], buy[i - 1] + p[i - 1] - p[i]);
-    }
-    return ret;
-}
-};
+buy[i]表示在第i天之前最后一个操作是买，此时的最大收益。
+
+sell[i]表示在第i天之前最后一个操作是卖，此时的最大收益。
+
+rest[i]表示在第i天之前最后一个操作是冷冻期，此时的最大收益。
+
+我们写出递推式为：
+
+buy[i]  = max(rest[i-1] - price, buy[i-1]) 
+sell[i] = max(buy[i-1] + price, sell[i-1])
+rest[i] = max(sell[i-1], buy[i-1], rest[i-1])
+
+由于冷冻期的存在，我们可以得出rest[i] = sell[i-1]，这样，我们可以将上面三个递推式精简到两个：
+
+buy[i]  = max(sell[i-2] - price, buy[i-1]) 
+sell[i] = max(buy[i-1] + price, sell[i-1])
+
+
+//=============
 /*buy[i-1]+prices[i] represents buy the stock on day i-1 and sell it on day i; 
   sell[i-1]-prices[i-1]+prices[i] represents you didn't sell the stock on day i-1 but sell it on day i (bought stock back and sell it on day i).
   sell[i-2]-prices[i] means sold the stock on day i-2 and buy it on day i (day i-1 is cooldown). 
   buy[i-1]+prices[i-1]-prices[i] means you didn't buy the stock on day i-1 but buy it on day i.
 
 
-/*class Solution {
-public:
-    int maxProfit(vector<int>& prices) 
-{
-    int buy= INT_MIN, sell_1=0,sell_2=0;
 
-    for (int i = 0; i < prices.size(); ++i)
-    {
-        buy =  i < 3 ? max(-prices[i], buy):max(sell_2-prices[i], buy);
-
-        sell_2 = sell_1;
-
-        sell_1 = max(sell_1, buy + prices[i]);
-    }
-
-    return sell_1;
-}
-};
 //Dynamic programming
