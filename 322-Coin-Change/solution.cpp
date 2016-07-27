fdf9302b564ab242a594e2b9dc81c109a6f8b203
Xiaoying <yqliving@gmail.com>
2016-07-27 16:04:20 -0700

    [322. Coin Change][Accepted]committed by LeetCode Extension

--- 322-Coin-Change/solution.cpp
+++ 322-Coin-Change/solution.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    int coinChange(vector<int>& coins, int amount)
+{
+	int noWay = numeric_limits<int>::max();
+	vector<int> amountToNumCoins(amount + 1, noWay);
+	amountToNumCoins[0] = 0;
+
+	for (int coin : coins)
+	{
+		for (int i = coin; i <= amount; ++i)
+		{
+			int prev = i - coin;
+			if (amountToNumCoins[prev] != noWay)
+			{
+				amountToNumCoins[i] = min(amountToNumCoins[i], amountToNumCoins[prev] + 1);
+			}
+		}
+	}
+
+	return (amountToNumCoins[amount] == noWay) ? -1 : amountToNumCoins[amount];
+}
+};
+
+/*class Solution {
+public:
     int coinChange(vector<int>& coins, int amount) {
         //int Max = amount + 1;
         vector<int> dp(amount + 1, INT_MAX);
