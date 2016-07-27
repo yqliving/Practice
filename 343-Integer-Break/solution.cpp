39de9de9b79ae1bfc7b32ccf95a6955da7c1e8ce
Xiaoying <yqliving@gmail.com>
2016-07-27 16:22:17 -0700

    [343. Integer Break][Accepted]committed by LeetCode Extension

--- 343-Integer-Break/solution.cpp
+++ 343-Integer-Break/solution.cpp
@@ -13,9 +13,23 @@ public:
         vector<int> dp(n + 1, 0);
         for(int i = 2; i <= n; i++) {
             for(int j = 1; j <= i / 2; j++) {
-                dp[i] = max(dp[i], max(j, dp[j]) * max(i - j, dp[i - j]));
+                dp[i] = max(dp[i], max(j, dp[j]) * max(i - j, dp[i - j])); //看看是现在的数大，还是把这个数拆分了大
             }
         }
         return dp[n];
+    }
+};
+
+/*
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n == 2 || n == 3) return n - 1;
+        int res = 1;
+        while (n > 4) {
+            res *= 3;
+            n -= 3;
+        }
+        return res * n;
     }
 };
