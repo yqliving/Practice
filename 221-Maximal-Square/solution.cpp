ea6c9ce47a0a6df429ac347a802431d5ee559519
Xiaoying <yqliving@gmail.com>
2016-07-27 14:28:22 -0700

    [221. Maximal Square][Accepted]committed by LeetCode Extension

--- 221-Maximal-Square/solution.cpp
+++ 221-Maximal-Square/solution.cpp
@@ -19,7 +19,7 @@ public:
                 
                 // i>0 && j>0 && matrix[i][j]=='1'
                 if (i!=0 && j!=0 & dp[i][j]!=0){
-                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1;
+                    dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
                 }
                 maxSize = max(maxSize, dp[i][j]);
                 //tracking the maxSize
