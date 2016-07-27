37dbf4cfe652d9499fa1e0789ce6cfb38a37869f
Xiaoying <yqliving@gmail.com>
2016-07-27 14:42:39 -0700

    [279. Perfect Squares][Accepted]committed by LeetCode Extension

--- 279-Perfect-Squares/solution.cpp
+++ 279-Perfect-Squares/solution.cpp
@@ -1,3 +1,20 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        if(n <= 0){
+            return 0;
+        }
+        vector<int> cnt(n + 1, INT_MAX);
+        cnt[0] = 0;
+        for(int i = 1; i <= n; ++i){
+            for(int j = 1; j * j <= i; ++j){
+                cnt[i] = min(cnt[i], cnt[i - j * j] + 1);
+            }
+        }
+        return cnt[n];
+    }
+};
+
 /*class Solution {  
 public:  
     int is_square(int n){  
@@ -50,7 +67,7 @@ public:
     }
 }; */
 
-//2.Static Dynamic Programming: 12ms
+/*2.Static Dynamic Programming: 12ms
 
 class Solution 
 {
