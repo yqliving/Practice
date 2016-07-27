3dff64f31cb61ba5089ac46fe73bdcdc9b5ff3ff
Xiaoying <yqliving@gmail.com>
2016-07-27 15:18:07 -0700

    [279. Perfect Squares][Accepted]committed by LeetCode Extension

--- 279-Perfect-Squares/solution.cpp
+++ 279-Perfect-Squares/solution.cpp
@@ -12,18 +12,18 @@ public:
         {
             return 0;
         }
-        vector<int> dp(n+1, INT_MAX);
-        // 将所有平方数的结果置1
-        for(int i = 0; i * i <= n; i++){
-            dp[i * i] = 1;
-        }
-        // 从小到大找任意数a
-        for(int a = 0; a <= n; a++){
-            // 从小到大找平方数bｘb
-            for(int b = 0; a + b * b <= n; b++){
-                // 因为a+b*b可能本身就是平方数，所以我们要取两个中较小的
-                dp[a + b * b] = min(dp[a] + 1, dp[a + b * b]);
+        //static vector<int> cntPerfectSquares({0});
+        static vector<int> dp({0});
+        while (dp.size() <= n)
+        {
+            int m = dp.size();
+            int dpSquares = INT_MAX;
+            for (int i = 1; i*i <= m; i++)
+            {
+                dpSquares = min(dpSquares, dp[m - i*i] + 1);
             }
+            
+            dp.push_back(dpSquares);
         }
         return dp[n];
     }
