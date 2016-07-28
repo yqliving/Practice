2273c7854578a6e871a9af400dbef7528b77ff82
Xiaoying <yqliving@gmail.com>
2016-07-28 14:02:29 -0700

    [191. Number of 1 Bits][Accepted]committed by LeetCode Extension

--- 191-Number-of-1-Bits/solution.cpp
+++ 191-Number-of-1-Bits/solution.cpp
@@ -3,7 +3,7 @@ public:
     int hammingWeight(uint32_t n) {
         int num = 0;
         for (int i = 0; i < 32; i++) {
-        if ((n >> i) & 1) == 1) num++;
+            if (((n >> i) & 1) == 1) num++;
     }
     return num;
         
