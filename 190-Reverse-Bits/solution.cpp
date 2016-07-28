0c27143095775e3b58900f1bfa8f82aad693cbd1
Xiaoying <yqliving@gmail.com>
2016-07-28 14:00:24 -0700

    [190. Reverse Bits][Accepted]committed by LeetCode Extension

--- 190-Reverse-Bits/solution.cpp
+++ 190-Reverse-Bits/solution.cpp
@@ -3,7 +3,7 @@ public:
     uint32_t reverseBits(uint32_t n) {
         auto r = 0;
         for (auto i = 0; i < 32; ++i) {
-            r = (r << 1) | ((n >> i) & 1);
+            r = (r << 1) | ((n >> i) & 1);   //r每次左移一个，((n >> i) & 1)每次取出一位的值
         }
         return r;
     }
