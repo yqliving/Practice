d38e1e4352facf51c8a4e07832d06ffdfd22bf03
Xiaoying <yqliving@gmail.com>
2016-07-28 15:34:01 -0700

    [147. Insertion Sort List][Accepted]committed by LeetCode Extension

--- 147-Insertion-Sort-List/solution.cpp
+++ 147-Insertion-Sort-List/solution.cpp
@@ -36,33 +36,7 @@ public:
     }
 };
 
-/*
-class Solution { 
-public:
-    ListNode* insertionSortList(ListNode* head) {
-        ListNode* new_head = new ListNode(0);
-        new_head -> next = head;
-        ListNode* pre = new_head;
-        ListNode* cur = head;
-        while (cur) {
-            if (cur -> next && cur -> next -> val < cur -> val) {
-                while (pre -> next && pre -> next -> val < cur -> next -> val)
-                    pre = pre -> next;
-                /* Insert cur -> next after pre.*/
-                ListNode* temp = pre -> next;
-                pre -> next = cur -> next;
-                cur -> next = cur -> next -> next;
-                pre -> next -> next = temp;
-                /* Move pre back to new_head. */
-                pre = new_head;
-            }
-            else cur = cur -> next;
-        }
-        ListNode* res = new_head -> next;
-        delete new_head;
-        return res;
-    }
-};
+
  /*
  class Solution{
      public:
