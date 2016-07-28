c0b683963a1d8622d82f75529cee91b2a3453141
Xiaoying <yqliving@gmail.com>
2016-07-28 15:34:27 -0700

    [147. Insertion Sort List][Accepted]committed by LeetCode Extension

--- 147-Insertion-Sort-List/solution.cpp
+++ 147-Insertion-Sort-List/solution.cpp
@@ -13,7 +13,34 @@ First, a quick recap of insertion sort:
 Start from the second element (simply a[1] in array and the annoying head -> next -> val in linked list), each time when we see a node with val smaller than its previous node, we scan from the head and find the position that the current node should be inserted. Since a node may be inserted before head, we create a new_head that points to head. The insertion operation, however, is a little easier for linked list.
 
 Now comes the code:*/
-class Solution {
+
+class Solution { 
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode* new_head = new ListNode(0);
+        new_head -> next = head;
+        ListNode* pre = new_head;
+        ListNode* cur = head;
+        while (cur) {
+            if (cur -> next && cur -> next -> val < cur -> val) {
+                while (pre -> next && pre -> next -> val < cur -> next -> val)
+                    pre = pre -> next;
+                /* Insert cur -> next after pre.*/
+                ListNode* temp = pre -> next;
+                pre -> next = cur -> next;
+                cur -> next = cur -> next -> next;
+                pre -> next -> next = temp;
+                /* Move pre back to new_head. */
+                pre = new_head;
+            }
+            else cur = cur -> next;
+        }
+        ListNode* res = new_head -> next;
+        delete new_head;
+        return res;
+    }
+};
+/*class Solution {
 public:
     ListNode *insertionSortList(ListNode *head) {
         ListNode *sortedHead = new ListNode(-1);
