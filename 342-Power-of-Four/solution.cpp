5aec1a062f7c8fd0ded78b89520c8bfd7d216736
Xiaoying <yqliving@gmail.com>
2016-07-28 14:52:07 -0700

    [342. Power of Four][Accepted]committed by LeetCode Extension

--- 342-Power-of-Four/solution.cpp
+++ 342-Power-of-Four/solution.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     bool isPowerOfFour(int num) {
         // first check only one bit is set:
-        if ((num & (num -1)) != 0) return false;
+        if ((num & (num -1)) != 0) return false;  //(n & (n - 1)) == 0; 代表是power of 2
         // next check if it's a bit in pos 1, 3, 5 ... 31
         if (num & 0x55555555) return true;  
         //0x55555555 十六进制数，是01010101010101010101010101010101
