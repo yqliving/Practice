bce143a3c386d95cb70f8224e69f0a5b5b76a38c
Xiaoying <yqliving@gmail.com>
2016-07-28 15:15:29 -0700

    [274. H-Index][Accepted]committed by LeetCode Extension

--- 274-H-Index/solution.cpp
+++ 274-H-Index/solution.cpp
@@ -1,3 +1,27 @@
+// 第二种的方法
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int citationSize = citations.size();
+        if (citationSize < 1) return 0;
+        vector<int> record(citationSize + 1, 0);
+        for (int i = 0; i < citationSize; ++i)
+        {
+            if (citations[i] <= citationSize)
+                ++record[citations[i]];
+            else
+                ++record[citationSize];
+        }
+
+        for (int j = citationSize, paperNum = 0; j >= 0; --j)
+        {
+            paperNum += record[j];
+            if (paperNum >= j) return j;
+        }
+        return 0;
+    }
+};
+/*
 // 排序+遍历
 class Solution {
 public:
@@ -10,3 +34,4 @@ public:
         return i;
     }
 };
+
