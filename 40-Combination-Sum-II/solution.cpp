81a3141a3fa25bc1ae9594cd02d2f2503ca2ebbe
Xiaoying <yqliving@gmail.com>
2016-07-30 11:20:35 -0700

    [40. Combination Sum II][Compile Error]committed by LeetCode Extension

--- 40-Combination-Sum-II/solution.cpp
+++ 40-Combination-Sum-II/solution.cpp
@@ -8,11 +8,12 @@ class Solution {
         return res;
     }
     void help(vector<int>& candidates, int target, int start, vector<vector<int>>& res, vector<int>& cur) {
-        if (target == 0 && candidates[i] == candidates[i-1]) {
+        if (target == 0) {
             res.push_back(cur);
             return;
         }
         for (int i = start; i < candidates.size() && candidates[i] <= target; i ++) {
+            if (i != start && candidates[i] == nums[i-1]) continue;
                 cur.push_back(candidates[i]);
                 help (candidates, target - candidates[i], i, res, cur);
                 cur.pop_back();
