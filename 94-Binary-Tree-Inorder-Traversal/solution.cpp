0376ba00590ad42b0cfdb58c33f125cc4579e727
Xiaoying <yqliving@gmail.com>
2016-07-30 16:35:49 -0700

    [94. Binary Tree Inorder Traversal][Compile Error]committed by LeetCode Extension

--- 94-Binary-Tree-Inorder-Traversal/solution.cpp
+++ 94-Binary-Tree-Inorder-Traversal/solution.cpp
@@ -19,8 +19,8 @@ class Solution {
             vector<int> left = inorderTraversal(root->left);
             left.push_back(root->val);
             vector<int> right = inorderTraversal(root->right);
-            
-            left.insert(left.end(), right.begin(), right.end());
+            left.push_back(right);
+            //left.insert(left.end(), right.begin(), right.end());
             return left;
         }
     };
