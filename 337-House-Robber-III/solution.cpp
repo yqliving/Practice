9843fc8d5b72283000cf4e0bb67a067e957e5e77
Xiaoying <yqliving@gmail.com>
2016-08-01 15:26:03 -0700

    [337. House Robber III][Compile Error]committed by LeetCode Extension

--- 337-House-Robber-III/solution.cpp
+++ 337-House-Robber-III/solution.cpp
@@ -8,23 +8,23 @@
  * };
  */
 class Solution {
-public:
-    int tryRob(TreeNode* root, int& l, int& r) {
-        if (!root)
-            return 0;
-            
-        int ll = 0, lr = 0, rl = 0, rr = 0;
-        l = tryRob(root->left, ll, lr);
-        r = tryRob(root->right, rl, rr);
-        
-        return max(root->val + ll + lr + rl + rr, l + r);
-    }
-
+    public:
     int rob(TreeNode* root) {
         int l, r;
-        return tryRob(root, l, r);
+        return tryRob (root, l, r);
+        
+        int tryRob(TreeNode* root, int l, int r) {
+            if (!root) return 0;
+            
+            int ll = lr = rl = rr = 0;
+            l = tryRob(root -> left, ll, lr);
+            r = tryRob(root -> right, rl, rr);
+            
+            return max(root->val + ll + lr + rl + rr, l + r);
+        }
     }
 };
+
 
 
 
