7b8dd2e8f0759ed6edce60aa0c36655f62e2c25c
Xiaoying <yqliving@gmail.com>
2016-08-01 10:39:10 -0700

    [111. Minimum Depth of Binary Tree][Accepted]committed by LeetCode Extension

---
+++ 111-Minimum-Depth-of-Binary-Tree/solution.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+    if (!root)   return 0;
+    else if (!root -> left)
+        return minDepth(root -> right) + 1;
+    else if (!root -> right)
+        return minDepth(root -> left) + 1;
+    else /* if (root.left != null && root.right != null) */
+        return min(minDepth(root -> left), minDepth(root -> right)) + 1;
+    }
+};
+//1. DFS: Time ~ O(N), Space ~ O(logN) 
+//注意minDepth与maxDepth的区别，当仅有一边子树为空时，minDepth是另一边子树的depth。
