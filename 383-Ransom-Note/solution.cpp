2e3a3e5ef5c802f32740bb900073d4d18341bb64
Xiaoying <yqliving@gmail.com>
2016-08-23 16:55:43 -0700

    [383. Ransom Note][Accepted]committed by LeetCode Extension

--- 383-Ransom-Note/solution.cpp
+++ 383-Ransom-Note/solution.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+        vector<int> vec(26, 0);
+        for (int i = 0; i < magazine.size(); ++i)
+            ++vec[magazine[i] - 'a'];
+        for (int j = 0; j < ransomNote.size(); ++j)
+            if (--vec[ransomNote[j] - 'a'] < 0)
+                return false;
+        return true;
+    }
+};
+/*class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char, int> magazLetters;
         for(char m : magazine)
             magazLetters[m]++;
