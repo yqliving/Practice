bb63bf633cd136a2d402a122cec764b21e3a2c5d
Xiaoying <yqliving@gmail.com>
2016-08-23 15:11:00 -0700

    [226. Invert Binary Tree][Compile Error]committed by LeetCode Extension

--- 226-Invert-Binary-Tree/solution.cpp
+++ 226-Invert-Binary-Tree/solution.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-public class Solution {  
+class Solution {  
     public:
     TreeNode* invertTree(TreeNode* root) {  
         if(!root) return null;  
@@ -21,8 +21,8 @@ public class Solution {
           
         return root;      
     }  
-      
-}  
+};
+
 /*
 class Solution {
 public:
