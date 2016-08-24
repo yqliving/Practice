ae09fac0dcbd2369e541e2a04989c095a5e3b6f1
Xiaoying <yqliving@gmail.com>
2016-08-24 11:08:31 -0700

    [387. First Unique Character in a String][Accepted]committed by LeetCode Extension

--- 387-First-Unique-Character-in-a-String/solution.cpp
+++ 387-First-Unique-Character-in-a-String/solution.cpp
@@ -1,17 +1,17 @@
-class Solution{
-    public:
+class Solution {
+public:
     int firstUniqChar(string s) {
-        if (s.size()==0) return -1;
-        vector<int> m(128, 0);
-        for (auto c:s)
-           m[c]++;
-        int i=0;   
-        for (;i<s.size();i++){
-            if (m[s[i]]==1) break;
+        vector<int> alphabet(26,0);
+        for (int i = 0; i < s.size(); i++){
+            alphabet[s[i] -'a']++;
         }
-        return (i==s.size()) ? -1 : i;
+        for (int i = 0; i < s.size(); i++) {
+            if (alphabet[s[i]-'a'] == 1) return i;
+        }
+        return -1;
     }
-};/*class Solution {
+};
+/*class Solution {
 public:
     int firstUniqChar(string s) {
         unordered_map<char, int> m;
