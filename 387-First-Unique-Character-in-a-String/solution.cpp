5a9fc1b4ede7a57cab333fda4c465e9fbfe825ff
Xiaoying <yqliving@gmail.com>
2016-08-24 11:01:35 -0700

    [387. First Unique Character in a String][Runtime Error]committed by LeetCode Extension

--- 387-First-Unique-Character-in-a-String/solution.cpp
+++ 387-First-Unique-Character-in-a-String/solution.cpp
@@ -6,7 +6,7 @@ class Solution{
             counts[m - 'a']++;
         }
         for (int i = 0; i < s.size(); i++) {
-            if(counts[s[i]]==1) return i;
+            if(counts[s[i] - 'a']==1) return i;
         }
         return -1;
     }
