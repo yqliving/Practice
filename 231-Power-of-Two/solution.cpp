35b66f9b26db1df9c90e20c19c0d0259c5596a25
Xiaoying <yqliving@gmail.com>
2016-08-25 12:22:28 -0700

    [231. Power of Two][Accepted]committed by LeetCode Extension

--- 231-Power-of-Two/solution.cpp
+++ 231-Power-of-Two/solution.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;   // bits method
+        return fmod(log10(n)/log10(2), 1)==0;
+        //return n > 0 && (n & (n - 1)) == 0;   // bits method
         //return fmod(log10(n)/log10(2), 1)==0;    //math method
     }
 };
