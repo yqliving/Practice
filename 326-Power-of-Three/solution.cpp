99c5cfdc93cf6ef4fa58f253b61210a2b4f58928
Xiaoying <yqliving@gmail.com>
2016-08-25 12:09:29 -0700

    [326. Power of Three][Compile Error]committed by LeetCode Extension

--- 326-Power-of-Three/solution.cpp
+++ 326-Power-of-Three/solution.cpp
@@ -1,20 +1,8 @@
+
 class Solution {
-    public:
-    bool isPowerOfThree(int n) {
-        if (n==0) return false;
-        int i=0;
-        for(; 3^i < n; i++){
-            if (n == 3^(i+1))) return true;
-            if(3^(i+1) > n) break;
-        }
-        return false;
-        
-    }
-};
-/*class Solution {
 public:
     bool isPowerOfThree(int n) {
-        return fmod(log10(n)/log10(3), 1)==0;
+        return (log10(n)/log10(3) % 1)==0;
         
     }
 };
