40f789b4368946a2f3f87ab5fb334e9a94821f4f
Xiaoying <yqliving@gmail.com>
2016-09-01 15:37:25 -0700

    [299. Bulls and Cows][Accepted]committed by LeetCode Extension

--- 299-Bulls-and-Cows/solution.cpp
+++ 299-Bulls-and-Cows/solution.cpp
@@ -1,30 +1,26 @@
-/*class Solution {
+class Solution {
 public:
     // only contains digits 
     string getHint(string secret, string guess) {
         int aCnt = 0;
         int bCnt = 0;
-        vector<int> sVec(10, 0); // 0 ~ 9 for secret
-        vector<int> gVec(10, 0); // 0 ~ 9 for guess 
+        vector<int> Vec(10, 0); 
         if (secret.size() != guess.size() || secret.empty()) { return "0A0B"; }
         for (int i = 0; i < secret.size(); ++i) {
             char c1 = secret[i]; char c2 = guess[i];
             if (c1 == c2) {
                 ++aCnt; 
             } else {
-                ++sVec[c1-'0'];
-                ++gVec[c2-'0'];
+                if (Vec[c1-'0']++ < 0) bCnt++;
+                if (Vec[c2-'0']-- > 0) bCnt++;
             }
         }
-        // count b 
-        for (int i = 0; i < sVec.size(); ++i) {
-            bCnt += min(sVec[i], gVec[i]);
-        }
+        
         return to_string(aCnt) + 'A' + to_string(bCnt) + 'B';
     }
-};*/
+};
 
-
+/*
 class Solution {
 public:
     string getHint(string secret, string guess) {
