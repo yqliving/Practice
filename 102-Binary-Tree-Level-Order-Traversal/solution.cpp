3f18473c5ca825990e0df0fd434403abd89771a9
Xiaoying <yqliving@gmail.com>
2016-09-01 14:15:58 -0700

    [102. Binary Tree Level Order Traversal][Accepted]committed by LeetCode Extension

--- 102-Binary-Tree-Level-Order-Traversal/solution.cpp
+++ 102-Binary-Tree-Level-Order-Traversal/solution.cpp
@@ -20,8 +20,6 @@ public:
     void Build(TreeNode* root,int layer){
         if(!root) return;
         if(layer==res.size()) res.push_back(vector<int>());
-        
-        
         res[layer].push_back(root->val);
         Build(root->left,layer+1);
         Build(root->right,layer+1);
