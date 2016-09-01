5d3b52e0d503357e19fe437f05d7520e233faee3
Xiaoying <yqliving@gmail.com>
2016-09-01 14:08:42 -0700

    [102. Binary Tree Level Order Traversal][Compile Error]committed by LeetCode Extension

--- 102-Binary-Tree-Level-Order-Traversal/solution.cpp
+++ 102-Binary-Tree-Level-Order-Traversal/solution.cpp
@@ -8,25 +8,25 @@
  * };
  */
 class Solution {
-    public:
-    vector<vector<int>> ret;
-
-void buildVector(TreeNode *root, int depth)
-{
-    if(root == NULL) return;
-    if(ret.size() == depth)
-        ret.push_back(vector<int>());
     
-    ret[depth].push_back(root->val);
-    buildVector(root->left, depth + 1);
-    buildVector(root->right, depth + 1);
-}
-
-vector<vector<int> > levelOrder(TreeNode *root) {
-    buildVector(root, 0);
-    return ret;
-}
+public:
+    vector<vector<int>> res;
+    
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        Build(root,0);
+        return res;
+    }
+    
+    void Build(TreeNode* root,int layer){
+        if(!root) return;
+        if(layer==res.size()) res.push_back(vector<int>{root->val});
+        else res[layer].push_back(root->val);
+        Build(root->left,layer+1);
+        Build(root->right,layer+1);
+        return;
+    }
 };
+
 /*
 class Solution {
 public:
