8b2550e62cae48efb72d1d50d6d069e37ed389d1
Xiaoying <yqliving@gmail.com>
2016-09-01 11:08:23 -0700

    [70. Climbing Stairs][Accepted]committed by LeetCode Extension

--- 70-Climbing-Stairs/solution.cpp
+++ 70-Climbing-Stairs/solution.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        if(n == 0 || n == 1 || n == 2){return n;}
         vector<int> a(50);
-        a[0]=1;a[1]=2;
+        a[0] = 1, a[1] = 2;
         for(int i=2;i<n;i++){
             a[i]=a[i-1]+a[i-2];
         }
