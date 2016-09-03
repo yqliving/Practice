d2ad74cb3ab2b7aa3f915a3d98dd887bb24bd942
Xiaoying <yqliving@gmail.com>
2016-09-02 21:55:39 -0700

    [204. Count Primes][Accepted]committed by LeetCode Extension

--- 204-Count-Primes/solution.cpp
+++ 204-Count-Primes/solution.cpp
@@ -5,7 +5,7 @@ class Solution {
      //2,3,5,7,11,13,17   
      //20  5  
      //init check  n  
-     bool a = new bool[n];  
+     bool *a = new bool[n];  
      for(int i=2; i*i<n; i++) {  
         if(!a[i]) {  
             for(int j=i; i*j<n; j++) {  
