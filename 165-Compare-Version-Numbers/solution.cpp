ae9a753b7691dda2e8b3f3f06a4650c4d6832e6d
Xiaoying <yqliving@gmail.com>
2016-09-02 22:27:27 -0700

    [165. Compare Version Numbers][Accepted]committed by LeetCode Extension

--- 165-Compare-Version-Numbers/solution.cpp
+++ 165-Compare-Version-Numbers/solution.cpp
@@ -18,3 +18,37 @@ public:
     	return 0;
     }
 };
+
+/*以小数点为分隔符，先取出最前面的数字部分，并转换成整数。
+
+如果两个版本对应的整数不等，则直接给出比较结果。
+如果两个版本对应的整数相等，那就再比较版本后面的部分
+
+class Solution
+{
+public:
+    int compareVersion(string version1, string version2)
+    {
+        while (!version1.empty() || !version2.empty())
+        {
+            auto ver1 = version1.empty() ? 0 : stoi(version1);
+            auto ver2 = version2.empty() ? 0 : stoi(version2);
+            if (ver1 < ver2)
+            {
+                return -1;
+            }
+            else if (ver1 > ver2)
+            {
+                return 1;
+            }
+            else
+            {
+                auto pos1 = version1.find('.');
+                version1 = (pos1 == string::npos) ? "" : version1.substr(pos1 + 1);
+                auto pos2 = version2.find('.');
+                version2 = (pos2 == string::npos) ? "" : version2.substr(pos2 + 1);
+            }
+        }
+        return 0;
+    }
+};
