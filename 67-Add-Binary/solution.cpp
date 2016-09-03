5697dc3ba280b719c95512b7af0d6553cf2aeec2
Xiaoying <yqliving@gmail.com>
2016-09-02 21:28:58 -0700

    [67. Add Binary][Accepted]committed by LeetCode Extension

--- 67-Add-Binary/solution.cpp
+++ 67-Add-Binary/solution.cpp
@@ -9,9 +9,9 @@ class Solution {
         while (i >= 0 || j >=0 || carry == 1) {
             carry += i >= 0 ? a[i--] - '0' : 0;
             carry += j >= 0 ? b[j--] - '0' : 0;
-            res += char(carry % 2 + '0');
+            res = char(carry % 2 + '0') + res;
             carry /= 2;
         }
-        return s;
+        return res;
     }
 };
