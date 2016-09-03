15b65c9d89f870a6c1b8d4b121f429498f46e9ca
Xiaoying <yqliving@gmail.com>
2016-09-02 22:18:01 -0700

    [189. Rotate Array][Compile Error]committed by LeetCode Extension

--- 189-Rotate-Array/solution.cpp
+++ 189-Rotate-Array/solution.cpp
@@ -18,8 +18,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k %= n;
-        reverse(nums, nums+(n-k));
-        reverse(nums+(n-k), nums+n);
-        reverse(nums, nums+n);
+        reverse(nums.begin(), nums.end()-k));
+        reverse(nums.end()-k, nums.end());
+        reverse(nums.begin(), nums.end());
     }
 };
