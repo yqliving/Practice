f16820e5f6ef9a973ac44bc88cc9a5417147721c
Xiaoying <yqliving@gmail.com>
2016-09-02 21:28:29 -0700

    [67. Add Binary][Compile Error]committed by LeetCode Extension

--- 67-Add-Binary/solution.cpp
+++ 67-Add-Binary/solution.cpp
@@ -3,13 +3,13 @@
 class Solution {
     public:
     string addBinary (string a, string b) {
-        string s = "";
+        string res = "";
         int carry = 0;
         int i = a.size() - 1, j = b.size() - 1;
         while (i >= 0 || j >=0 || carry == 1) {
             carry += i >= 0 ? a[i--] - '0' : 0;
             carry += j >= 0 ? b[j--] - '0' : 0;
-            s = char(carry % 2 + '0') + s;
+            res += char(carry % 2 + '0');
             carry /= 2;
         }
         return s;
